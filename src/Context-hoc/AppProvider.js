9b67e00dbae4c5e607948c51b2ed20330fc78792
Ilingu <thetitouoff@gmail.com>
2020-08-26 18:13:45 +0200

    Add Principale dependance + file architecture

---
+++ src/Context-hoc/AppProvider.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+// Fn
+import { GETMusic } from "../includes/fonctions";
+// Context
+import AppContext from "./AppContext";
+
+class AppProvider extends Component {
+  state = {
+    // Les info de toute les music dans AllMusicInfo et quand cliker sur une on demande le mp3 et on le met dans musicPartNow
+    AllMusicInfo: {},
+    musicPartNow: {},
+  };
+
+  componentDidMount() {
+    // Call
+  }
+
+  render() {
+    return (
+      <AppContext.Provider state={this.state}>
+        {this.props.children}
+      </AppContext.Provider>
+    );
+  }
+}
+
+export default AppProvider;
